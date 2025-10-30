8132eeef5750e64ee9ba15f52826022a183b4a0b
AkeemGit <500002961@my.fiveislands.uwi.edu>
2025-10-29 21:02:28 -0400

    Added javascript functionality

--- scripts.js
+++ scripts.js
@@ -1,1 +1,35 @@
 /* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded',function(){
+    // let button = document.querySelectorAll('.btn')[1];
+    let form = document.querySelector('form');
+    let message = document.querySelector('.message');
+    let emailInput = document.getElementById('email');
+
+
+    function isValidEmail(email){
+        const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        return re.test(email);
+    }
+
+
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+        
+        let email = emailInput.value.trim();
+        
+        if (email == ''){
+            message.innerHTML = `<b><p>Please enter an email address</p></b>`;
+            message.style.display = 'inline-block';
+        }
+        else if (isValidEmail(email)){
+            message.style.textAlign = 'center';
+            message.innerHTML = `<b><p>Thank you! Your email address ${email} has been added to our mailing list!</p></b>`;
+            
+
+        }else{
+            message.innerHTML = `<b><p>Please enter a valid email address"</p></b>`;
+            message.style.display = 'inline-block';
+        }  
+    });
+
+});
